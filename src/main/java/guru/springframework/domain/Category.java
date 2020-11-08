774c7015a47531ddd3e33227eca0e20a2ded528d
madalinalzr <72612476+madalinalzr@users.noreply.github.com>
2020-11-08 22:09:10 +0200

    migrate from junit4 to junit5

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -22,4 +22,11 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
